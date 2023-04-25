d2e2f0d5a7231d518af9651f036849304d57cf7c
MartaDorokhova <112471191+MartaDorokhova@users.noreply.github.com>
2023-04-25 22:30:10 +0300

    Add files via upload
    25/04

--- dom.js
+++ dom.js
@@ -1,103 +1,103 @@
 const tasks = [
-  {
-      id: '1138465078061',
-      completed: true,
-      text: 'Посмотреть новый урок по JavaScript',
-  },
-  {
-      id: '1138465078062',
-      completed: false,
-      text: 'Выполнить тест после урока',
-  },
-  {
-      id: '1138465078063',
-      completed: false,
-      text: 'Выполнить ДЗ после урока',
-  },
+    {
+        id: '1138465078061',
+        completed: true,
+        text: 'Посмотреть новый урок по JavaScript',
+    },
+    {
+        id: '1138465078062',
+        completed: false,
+        text: 'Выполнить тест после урока',
+    },
+    {
+        id: '1138465078063',
+        completed: false,
+        text: 'Выполнить ДЗ после урока',
+    },
 ];
 
 const createTaskItem = (taskId, taskText, taskCompleted) => {
-  const taskItem = document.createElement("div");
-  taskItem.className = "task-item";
-  taskItem.dataset.taskId = taskId;
+    const taskItem = document.createElement("div");
+    taskItem.className = "task-item";
+    taskItem.dataset.taskId = taskId;
+  
+    const taskItemMainContainer = document.createElement("div");
+    taskItemMainContainer.className = "task-item__main-container";
+  
+    const taskItemMainContent = document.createElement("div");
+    taskItemMainContent.className = "task-item__main-content";
+  
+    taskItem.append(taskItemMainContainer);
+    taskItemMainContainer.append(taskItemMainContent);
+  
+    const checkboxForm = document.createElement("form");
+    checkboxForm.className = "checkbox-form";
+  
+  
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.className = "checkbox-form__checkbox";
+    const inputId = `tasks-${taskId}`;
+    inputCheckbox.id = inputId;
+    inputCheckbox.checked = taskCompleted;
+  
+    const labelCheckbox = document.createElement("label");
+    labelCheckbox.htmlFor = inputId;
+  
+    const taskItemText = document.createElement("span");
+    taskItemText.className = "task-item__text";
+    taskItemText.innerText = taskText;
+  
+    const deleteButton = document.createElement("button");
+    deleteButton.className =
+      "task-item__delete-button default-button delete-button";
+    deleteButton.innerText = "Удалить";
+  
+    taskItemMainContent.append(checkboxForm, taskItemText);
+    checkboxForm.append(inputCheckbox, labelCheckbox);
+    taskItemMainContainer.append(deleteButton);
+  
+    return taskItem;
+  };
+  const errorText = (message) => {
 
-  const taskItemMainContainer = document.createElement("div");
-  taskItemMainContainer.className = "task-item__main-container";
+    const error = document.createElement("span");
+    error.textContent = message;
+  error.className = "error-message-block";
+    return error}
 
-  const taskItemMainContent = document.createElement("div");
-  taskItemMainContent.className = "task-item__main-content";
-
-  taskItem.append(taskItemMainContainer);
-  taskItemMainContainer.append(taskItemMainContent);
-
-  const checkboxForm = document.createElement("form");
-  checkboxForm.className = "checkbox-form";
-
-
-  const inputCheckbox = document.createElement("input");
-  inputCheckbox.type = "checkbox";
-  inputCheckbox.className = "checkbox-form__checkbox";
-  const inputId = `tasks-${taskId}`;
-  inputCheckbox.id = inputId;
-  inputCheckbox.checked = taskCompleted;
-
-  const labelCheckbox = document.createElement("label");
-  labelCheckbox.htmlFor = inputId;
-
-  const taskItemText = document.createElement("span");
-  taskItemText.className = "task-item__text";
-  taskItemText.innerText = taskText;
-
-  const deleteButton = document.createElement("button");
-  deleteButton.className =
-    "task-item__delete-button default-button delete-button";
-  deleteButton.innerText = "Удалить";
-
-  taskItemMainContent.append(checkboxForm, taskItemText);
-  checkboxForm.append(inputCheckbox, labelCheckbox);
-  taskItemMainContainer.append(deleteButton);
-
-  return taskItem;
-};
-const errorText = (message) => {
-
-  const error = document.createElement("span");
-  error.textContent = message;
-error.className = "error-message-block";
-  return error}
-
-const tasksListContainer = document.querySelector(".tasks-list");
-const render =() =>{
-  tasksListContainer.innerHTML =""
-  tasks.forEach((task) => {
-  const taskItem = createTaskItem(task.id, task.text, task.completed);
-  tasksListContainer.append(taskItem)
-});
+  const tasksListContainer = document.querySelector(".tasks-list");
+  const render =() =>{
+    tasksListContainer.innerHTML =""
+    tasks.forEach((task) => {
+    const taskItem = createTaskItem(task.id, task.text, task.completed);
+    tasksListContainer.append(taskItem)
+  });
 }
 render();
 
 const createForm = document.querySelector(`.create-task-block`);
 
 createForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const textValue = event.target.taskName.value;
-  
-  const errorHTML = document.querySelector('.error-message-block')
-  if(errorHTML) {
-      errorHTML.remove();
-  }
-  if(textValue === "") {
-     event.target.append(errorText('Название задачи не должно быть пустым'))
-  }
+    event.preventDefault();
+    const textValue = event.target.taskName.value;
+    
+    const errorHTML = document.querySelector('.error-message-block')
+    if(errorHTML) {
+        errorHTML.remove();
+    }
+    if(textValue === "") {
+       event.target.append(errorText('Название задачи не должно быть пустым'))
+    }
 
-  else if(tasks.some((task)=>task.text.toLowerCase() === textValue.toLowerCase())) {
-      event.target.append(errorText('Название задачи не должно повторяться'))
+    else if(tasks.some((task)=>task.text.toLowerCase() === textValue.toLowerCase())) {
+        event.target.append(errorText('Название задачи не должно повторяться'))
 
 } 
-   else if (textValue) {
-      tasks.push({id:Date.now(), completed: false, text: textValue });
-      render()
-   }
+     else if (textValue) {
+        tasks.push({id:Date.now(), completed: false, text: textValue });
+        render()
+     }
 });
 
 const modalHidden = document.createElement('div');
@@ -127,16 +127,33 @@ deleteModalButton.append(buttonYes);
 
 const clickButton = document.querySelector(".tasks-list");
 clickButton.addEventListener("click", (event) => {
-  modalHidden.className = 'modal-overlay';
-})
+    const idFind = event.target.closest('.task-item').dataset.taskId;
+    modalHidden.className = 'modal-overlay';
+    modalHidden.dataset.taskId = idFind;
+    })
 
 buttonNo.addEventListener("click", (event) => {
-  modalHidden.className = 'modal-overlay modal-overlay_hidden';
-  console.log('Отмена удаления')
+    modalHidden.className = 'modal-overlay modal-overlay_hidden';
+    console.log('Отмена удаления')
 })
 buttonYes.addEventListener("click", (event) => {
-  console.log('Удаление')
-  modalHidden.className = 'modal-overlay modal-overlay_hidden';
-  taskItem.remove()
-      })
 
+    const element = event.target;
+    console.log(element);
+    const id = element.closest('.modal-overlay').dataset.taskId;
+    console.log(id);
+    const found = tasks.indexOf(tasks.find(task => task.id === id));
+    const resultSplice = tasks.filter(task => task.id !== id);
+    console.log(resultSplice);
+    modalHidden.className = 'modal-overlay modal-overlay_hidden';
+    const render =() =>{
+        const tasksListContainer = document.querySelector(".tasks-list");
+        tasksListContainer.innerHTML =""
+        resultSplice.forEach((task) => {
+        const taskItem = createTaskItem(resultSplice.id, resultSplice.text, resultSplice.completed);
+        tasksListContainer.append(taskItem)
+        console.log(taskItem)
+      });}
+      render();
+        })
+    
